cc68473e24bf42df9aa7012ec6b3c3187bdd1bb4
Verdio - Rovema Bank <luiz.bicalho@rovemabank.com.br>
2025-10-30 09:32:46 -0400

    Update firebase_config.py

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,7 +1,7 @@
+# utils/firebase_config.py
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
-import pyrebase
 
 # Carrega as credenciais do Streamlit Secrets
 creds_dict = st.secrets["firebase_service_account"]
@@ -23,20 +23,9 @@ def init_firebase_admin():
     
     return firestore.client()
 
-@st.cache_resource
-def init_pyrebase():
-    """
-    Inicializa o Pyrebase para operações de cliente (Login).
-    """
-    return pyrebase.initialize_app(firebase_config)
-
 def get_db():
     """Retorna a instância do cliente Firestore."""
     return init_firebase_admin()
-
-def get_auth_client():
-    """Retorna a instância do cliente de autenticação Pyrebase."""
-    return init_pyrebase().auth()
 
 def get_admin_auth():
     """Retorna o módulo de autenticação do Admin SDK."""
